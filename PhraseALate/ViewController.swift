3650314315c4fa0fbf02c88ad0e8026cf7b86ad9
Thomas Martin <tjmartin2@student.fullsail.edu>
2019-03-30 09:33:40 -0400

    Initial Commit

---
+++ PhraseALate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhraseALate
+//
+//  Created by Thomas Martin on 3/30/19.
+//  Copyright © 2019 Thomas Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
