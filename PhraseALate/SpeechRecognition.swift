c06bc07bf9b07b134aeb9090251bb9707fb8fc31
Thomas Martin <tjmartin2@student.fullsail.edu>
2019-05-09 10:03:01 -0400

    Added Speech Recognition ViewControllers

---
+++ PhraseALate/SpeechRecognition.swift
@@ -0,0 +1,30 @@
+//
+//  SpeechRecognition.swift
+//  PhraseALate
+//
+//  Created by Thomas Martin on 5/9/19.
+//  Copyright © 2019 Thomas Martin. All rights reserved.
+//
+
+import UIKit
+
+class SpeechRecognition: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
